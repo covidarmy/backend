3f5c86e1afb23635612897eecf8e51701cd65c23
Anshul <ans29hul@gmail.com>
2021-05-01 01:08:02 +0530

    Basic Swagger setup

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,12 +5,71 @@ const router = express.Router();
 
 module.exports = router;
 
+/**
+ * @swagger
+ * /:
+ *  get:
+ *    description: Simple test endpoint
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
 router.get("/", async (req, res) => {
   res.send("This is the API endpoint");
 });
 
+/**
+ * @swagger
+ * /tweets:
+ *  get:
+ *    description: Simple test endpoint
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
+
 router.get("/tweets", tweetController.findAll);
+
+/**
+ * @swagger
+ * /tweets/:location:
+ *  get:
+ *    description: Simple test endpoint
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
+
 router.get("/tweets/:location", tweetController.findAll);
+
+/**
+ * @swagger
+ * /tweets:
+ *   get:
+ *     summary: Retrieve a list tweets.
+ *     description: Retrieve a list of tweets.
+ *     responses:
+ *       200:
+ *         description: A list of tweets.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         description: The user ID.
+ *                         example: 0
+ *                       name:
+ *                         type: string
+ *                         description: The user's name.
+ *                         example: Leanne Graham
+ */
 router.get("/tweets/:location/:resource", tweetController.findAll);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const cron = require("node-cron");
 const express = require("express");
 const app = express();
 
+
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
 const morgan = require("morgan");
 const mongoose = require("mongoose");
 
@@ -17,6 +21,29 @@ mongoose
   .then(() => {
     console.log("✅ Databse Connected!");
   });
+  
+  // Swagger config
+  const swaggerOptions = {
+    swaggerDefinition: {
+      info: {
+        version: "1.0.0",
+        title: "covid.army API",
+        description: "Covid.army API Information",
+        contact: {
+          name: "API Support",
+          url: "https://twitter.com/covid_army"
+        },
+        servers: ["http://covid.army"]
+      }
+    },
+    // ['.routes/*.js']
+    apis: ["routes/apiRoutes.js"]
+  };
+
+//setup Swagger for auto documentation
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 
 //Import Routes
 const apiRoutes = require("./routes/apiRoutes");
@@ -40,6 +67,7 @@ app.use((req, res, next) => {
 //Express Routes
 app.use("/api", apiRoutes);
 
+// Routes
 app.use("/", async (req, res) => {
   res.send("Hello World!");
 });
