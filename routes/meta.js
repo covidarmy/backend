845b52215681e57494b03814ea498210555470c2
Swarnim Walavalkar <swarnim.walavalkar@gmail.com>
2021-05-04 14:56:10 +0530

    update swagger docs

--- routes/meta.js
+++ routes/meta.js
@@ -1,25 +1,37 @@
 const fs = require("fs");
 const express = require("express");
 
-const cities = require("fs").readFileSync("./data/cities.csv", "utf8").split("\n").map(row => row.split(","));
+const cities = require("fs")
+    .readFileSync("./data/cities.csv", "utf8")
+    .split("\n")
+    .map((row) => row.split(","));
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /api/checkCity:
+ *  get:
+ *    description: Check if city name is valid
+ *    responses:
+ *      '200':
+ *        description: A response object with `found` boolean field and name locale code
+ */
 router.get("/checkCity", (req, res) => {
-	let { city } = req.query;
+    let { city } = req.query;
 
-	if(!city){
-		return res.status(400).send({ error: "City not provided." });
-	}
+    if (!city) {
+        return res.status(400).send({ error: "City not provided." });
+    }
 
-	city = city.toLowerCase();
+    city = city.toLowerCase();
 
-	for(const [en, hi] of cities){
-		if(city == en.toLowerCase() || city == hi){
-			return res.send({ found: true, name: en });
-		}
-	}
-	return res.send({ found: false });
+    for (const [en, hi] of cities) {
+        if (city == en.toLowerCase() || city == hi) {
+            return res.send({ found: true, name: en });
+        }
+    }
+    return res.send({ found: false });
 });
 
 module.exports = router;
