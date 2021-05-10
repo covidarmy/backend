fa890be6b3b1f6083b0d127f3a34de2e91332d1c
Anshul <ans29hul@gmail.com>
2021-05-10 18:28:24 +0530

    Added CRON job to delete fraud tweets from fraud db

--- server.js
+++ server.js
@@ -50,8 +50,8 @@ const meta = require("./routes/meta");
 //Import the fetchTweets script
 const { fetchAndSaveTweets } = require("./fetchTweets");
 
-//Import the fetchTweets script
-const { deleteTweets } = require("./deleteTweets");
+//Import the deleteTweets script
+const { deleteTweets, deleteFraud } = require("./deleteTweets");
 
 //Express options
 app.use(morgan(process.env.NODE_ENV == "production" ? "common" : "dev"));
@@ -74,6 +74,13 @@ if (process.env.NODE_ENV === "production") {
         console.log("Fetching Tweets...");
         await fetchAndSaveTweets();
         console.log("Done Fetching Tweets!");
+    });
+    
+    //Schedule task to run every 6 hrs.
+    cron.schedule("*/360 * * * *", async () => {
+        console.log("Deleting fraud Tweets...");
+        await deleteFraud();
+        console.log("Done eleting fraud Tweets!");
     });
 }
 
