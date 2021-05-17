81985d163ce9c76a873d1a8f6eed3e13ba9d2cf1
Arnav Gosain <arnav@arnavgosain.com>
2021-05-17 21:02:47 +0530

    Add firebase admin
    Signed-off-by: Arnav Gosain <arnav@arnavgosain.com>

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,102 +1,40 @@
-const express = require("express");
-const { API_HOST, APP_DOMAIN } = require("../constants");
-const { createVerifier, createSigner } = require("fast-jwt");
-const simpleOauth2 = require("simple-oauth2");
-const crypto = require("crypto");
-const fetch = require("node-fetch");
-const util = require("util");
-const User = require("../models/User.schema");
-const html = String.raw;
+const { Router } = require("express");
+const { auth } = require("firebase-admin");
+/**
+ * @type {Router}
+ */
+const router = new Router();
 
-const randomBytes = util.promisify(crypto.randomBytes);
-const jwtSign = createSigner({ key: async () => process.env.JWT_SECRET });
-const jwtVerify = createVerifier({ key: async () => process.env.JWT_SECRET });
-
-/** @type {import('express').Router} */
-const router = new express.Router();
-
-const googleOauth2 = new simpleOauth2.AuthorizationCode({
-  client: {
-    id: process.env.GOOGLE_CLIENT_ID,
-    secret: process.env.GOOGLE_CLIENT_SECRET,
-  },
-  auth: {
-    authorizeHost: "https://accounts.google.com",
-    authorizePath: "/o/oauth2/v2/auth",
-    tokenHost: "https://www.googleapis.com",
-    tokenPath: "/oauth2/v4/token",
-  },
-});
-
-const redirect_uri = API_HOST + "/api/auth/google/callback";
-
-router.get("/auth/google", async (req, res) => {
-  const state = (await randomBytes(10)).toString("hex");
-  const authorizeUri = googleOauth2.authorizeURL({
-    redirect_uri,
-    scope: "openid email profile",
-    state,
-  });
-  res.redirect(authorizeUri);
-});
-
-const createUser = async (profile) => {
-  await User.create({
-    email: profile.email,
-  });
-  const user = await User.findOne({
-    email: profile.email,
-  });
-  return { sub: user._id };
+/**
+ * @type {import("express").Handler}
+ */
+const protectedHandler = async (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(400).json({ message: "You did not specify idToken." });
+  } else {
+    try {
+      const user = await auth().verifyIdToken(token);
+      if (user) {
+        req.user = user;
+        next();
+      } else {
+        res.status(400).send({ message: "Unable to verify user." });
+      }
+    } catch {
+      res.status(500).send({ message: "Unable to verify user." });
+    }
+  }
 };
 
-router.get("/auth/google/callback", async (req, res) => {
-  const code = req.query.code;
-  const result = await googleOauth2.getToken({
-    code,
-    redirect_uri,
-    scope: "openid email profile",
-  });
-
-  let claims = {};
-
-  const profile = await fetch(
-    `https://www.googleapis.com/oauth2/v3/userinfo?access_token=${result.token.access_token}`
-  ).then((res) => res.json());
-
-  try {
-    const user = await User.findOne({
-      email: profile.email,
-    });
-    if (user) {
-      claims = { sub: user._id };
+router
+  .route("/volunteer/leads")
+  .get(protectedHandler, async (req, res) => {
+    if (req.user) {
     } else {
-      claims = await createUser(profile);
     }
-  } catch (error) {
-    res.status(500).send({ error });
-  }
+  })
+  .post(protectedHandler, async (req, res) => {});
 
-  const token = await jwtSign(claims);
 
-  res.send(html`
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <title>Login Successful</title>
-      </head>
-      <body>
-        <h1>Authorized</h1>
-        <p>You can close this window now</p>
-        <script>
-          let originUrl = window.location.origin;
-          localStorage.setItem("access_token", "${token}");
-          window.opener.postMessage("success", originUrl);
-          window.close();
-        </script>
-      </body>
-    </html>
-  `);
-});
-
-module.exports = router;
+module.exports = router
