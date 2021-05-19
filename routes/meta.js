d98b66afa9e2c57dfbd1c2ae22c638c9fe7977a4
Swarnim Walavalkar <swarnim.walavalkar@gmail.com>
2021-05-19 20:55:56 +0530

    update checkCity to only include ACTIVE contacts

--- routes/meta.js
+++ routes/meta.js
@@ -4,9 +4,9 @@ const express = require("express");
 const Contact = require("../models/Contact.schema");
 
 const citiesRaw = require("fs")
-    .readFileSync("./data/cities.csv", "utf8")
-    .split("\n")
-    .map((row) => row.split(","));
+  .readFileSync("./data/cities.csv", "utf8")
+  .split("\n")
+  .map((row) => row.split(","));
 
 const allCities = require("../data/newAllCities.json");
 const resources = require("../data/resources.json");
@@ -37,18 +37,18 @@ const router = express.Router();
  *
  */
 router.get("/cities", async (req, res) => {
-    let resCities = [];
+  let resCities = [];
 
-    for (const state in allCities) {
-        for (const city of allCities[state]) {
-            let cityObj = {};
-            cityObj.name = city.name;
-            cityObj.top = city.top;
-            cityObj.hindiName = city.hindiName;
-            resCities.push(cityObj);
-        }
+  for (const state in allCities) {
+    for (const city of allCities[state]) {
+      let cityObj = {};
+      cityObj.name = city.name;
+      cityObj.top = city.top;
+      cityObj.hindiName = city.hindiName;
+      resCities.push(cityObj);
     }
-    return res.status(200).send(resCities);
+  }
+  return res.status(200).send(resCities);
 });
 
 /**
@@ -61,7 +61,7 @@ router.get("/cities", async (req, res) => {
  *             description: An array of all available resources
  */
 router.get("/resources", async (req, res) => {
-    return res.status(200).send(resources);
+  return res.status(200).send(resources);
 });
 
 /**
@@ -80,45 +80,43 @@ router.get("/resources", async (req, res) => {
  *
  */
 router.get("/checkCity", async (req, res) => {
-    try {
-        let { city } = req.query;
+  try {
+    let { city } = req.query;
 
-        if (!city) {
-            return res.status(400).send({ error: "City not provided." });
-        }
-
-        reqCity = city.toLowerCase();
-
-        for (const state in allCities) {
-            for (const city of allCities[state]) {
-                if (
-                    reqCity == city.name.toLowerCase() ||
-                    reqCity == city.hindiName
-                ) {
-                    const totalContacts = await Contact.countDocuments({
-                        city: city.name,
-                    });
-                    return res.send({
-                        found: true,
-                        name: city.name,
-                        totalContacts,
-                    });
-                }
-            }
-        }
-
-        // for (const [en, hi] of citiesRaw) {
-        //     if (city == en.toLowerCase() || city == hi) {
-        //         const totalContacts = await Contact.countDocuments({
-        //             city: en,
-        //         });
-        //         return res.send({ found: true, name: en, totalContacts });
-        //     }
-        // }
-        return res.send({ found: false });
-    } catch (error) {
-        res.status(500).send({ error: error.message });
+    if (!city) {
+      return res.status(400).send({ error: "City not provided." });
     }
+
+    reqCity = city.toLowerCase();
+
+    for (const state in allCities) {
+      for (const city of allCities[state]) {
+        if (reqCity == city.name.toLowerCase() || reqCity == city.hindiName) {
+          const totalContacts = await Contact.countDocuments({
+            city: city.name,
+            status: "ACTIVE",
+          });
+          return res.send({
+            found: true,
+            name: city.name,
+            totalContacts,
+          });
+        }
+      }
+    }
+
+    // for (const [en, hi] of citiesRaw) {
+    //     if (city == en.toLowerCase() || city == hi) {
+    //         const totalContacts = await Contact.countDocuments({
+    //             city: en,
+    //         });
+    //         return res.send({ found: true, name: en, totalContacts });
+    //     }
+    // }
+    return res.send({ found: false });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
 });
 
 module.exports = router;
