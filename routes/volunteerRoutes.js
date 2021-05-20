8c5786f14913622db5bc2def64cb4fd7ae2c81fb
Swarnim Walavalkar <swarnim.walavalkar@gmail.com>
2021-05-20 21:10:37 +0530

    add vounteer get route, add userId to contact schema

--- routes/volunteerRoutes.js
+++ routes/volunteerRoutes.js
@@ -11,72 +11,11 @@ const Volunteer = require("../models/Volunteer.schema");
 const contactController = require("../controllers/contact");
 const fraudController = require("../controllers/fraud");
 
-/**
- * @swagger
- * /volunteer/contacts/:
- *     post:
- *         summary: Submit a contact
- *         description: Submit a contact entry
- *         parameters:
- *             - in: body
- *               name: data
- *               schema:
- *                  required:
- *                      - contact_no
- *                      - title
- *                      - resource_type
- *                      - city
- *                      - state
- *                  properties:
- *                      status
- *                      contact_no
- *                      email
- *                      title
- *                      resource_type
- *                      address
- *                      description
- *                      city
- *                      state
- *                      pincode
- *                      quantity_available
- *                      price
- *                      source_link
- *                      created_by
- *
- *
- *
- *         responses:
- *             200:
- *                 description: A generic response object.
- *                 schema:
- *                     type: object
- *                     properties:
- *                         ok:
- *                         type: boolean
- *             500:
- *                 description: An error object.
- *                 schema:
- *                 type: object
- *                 properties:
- *                     error:
- *                     type: string
- */
-router.post("/contacts/", auth, contactController.postContact);
+router.get("/contacts", auth, async (req, res) => {
+  res.send(await Contact.find({ userId: req.user.uid }));
+});
+router.post("/contacts", auth, contactController.postContact);
 
-/**
- * @swagger
- * /volunteer/fraud:
- *     post:
- *         summary: Add a new Fraud number
- *         description: Add a new Fraud number
- *         responses:
- *             200:
- *                 description: Genric Success Response
- *             400:
- *                 description: Inavlid Request
- *             500:
- *                 description: Internal Server Error
- */
 router.post("/fraud", auth, fraudController.postFraud);
 
 router.post("/login", async (req, res) => {
